fe3e159e30d45e77b9bcd931dd1609de1e260597
HQKhanhDaisy <huynhquockhanh0907@gmail.com>
2021-12-17 17:50:02 +0700

    update

---
+++ src/main/java/com/example/LiquibaseSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseSampleApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/StudentRepository.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@SuppressWarnings("ALL")
+@Repository
+public interface StudentRepository extends CrudRepository<Student, Long> {
+
+    @Query("SELECT name FROM Student s WHERE s.name LIKE %:studentName%")
+    String findByName(String studentName);
+
+
+}
